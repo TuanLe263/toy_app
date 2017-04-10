f604035fe3a50d28363cd803214ff24f306bbb35
TuanLe263 <tuanlm263ptit@gmail.com>
2017-04-10 14:44:24 +0700

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-	has_many: microposts
+	has_many :microposts
+	validates :name, presence: true  # Replace FILL IN with the right code.
+	validates :email, presence: true  # Replace FILL IN with the right code.
 end
